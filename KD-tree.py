5ce3c7ba01603e56fbcbdb9ce33141eea022c51e
ytsai43 <ytsai43@asu.edu>
2017-07-04 04:33:27 -0700

    eliminate useless region

--- KD-tree.py
+++ KD-tree.py
@@ -1,5 +1,6 @@
 from collections import namedtuple
 from pprint import pformat
+import numpy as np
 
 class Node(namedtuple('Node', 'location left_child right_child')):
     def __repr__(self):
@@ -9,9 +10,16 @@ def kdtree(range_list, depth=0):
     
     k = len(range_list) # cal number of dimensions
     axis = depth % k # Select axis based on depth so that axis cycles through all valid values
-
+    
+    
     if abs(range_list[axis][1] - range_list[axis][0]) < 2:  #Set the condition that the range cann't be divided 
-        return Node(
+        axis_hat = axis
+        axis = (axis + 1)%k
+        
+        while((abs(range_list[axis][1] - range_list[axis][0]) < 2 and axis != axis_hat)):
+            axis = (axis + 1)%k
+        if axis == axis_hat:
+            return Node(
                 location = range_list,
                 left_child= None,
                 right_child= None )
@@ -21,16 +29,33 @@ def kdtree(range_list, depth=0):
     right_range_list = range_list[:] 
     left_range_list[axis] = (range_list[axis][0],cuttingPoint)
     right_range_list[axis] = (cuttingPoint,range_list[axis][1],)
-# Create node and construct subtrees
+    
+    # Create node and construct subtrees
+    
+    left_randomPoint = np.random.uniform(left_range_list[axis][0],left_range_list[axis][1],2)
+    right_randomPoint = np.random.uniform(right_range_list[axis][0],right_range_list[axis][1],2)
+    print(left_randomPoint)
+    print(right_randomPoint)
+    # Create node and construct subtrees
+    if abs(left_randomPoint[0] - left_randomPoint[1]) < 1:
+        left_child = "Eliminated"
+    else:
+        left_child = kdtree(left_range_list, depth + 1)
+
+    if abs(right_randomPoint[0] - right_randomPoint[1]) < 1:
+        right_child = "Eliminated"
+    else:
+        right_child = kdtree(right_range_list, depth + 1)
+
     return Node(
     location = range_list,
-    left_child=kdtree(left_range_list, depth + 1),
-    right_child=kdtree(right_range_list, depth + 1)
-   )
+    left_child = left_child,
+    right_child = right_child
+    )
 
 def main():
     """Example usage"""
-    range_list = [(0,4),(0,8)]
+    range_list = [(4,16),(7,23)]
     tree = kdtree(range_list)
     print(tree)
 
